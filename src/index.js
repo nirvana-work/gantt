79592af8d94dacc2998dccad93bf34c8a9dabb77
Raghubansh Mani <raghu@nirvana.work>
2020-07-14 19:56:19 +0530

    increase buffer to 20 days

--- src/index.js
+++ src/index.js
@@ -242,8 +242,8 @@ export default class Gantt {
       this.gantt_start = date_utils.add(this.gantt_start, -2, 'year');
       this.gantt_end = date_utils.add(this.gantt_end, 2, 'year');
     } else {
-      this.gantt_start = date_utils.add(this.gantt_start, -10, 'day');
-      this.gantt_end = date_utils.add(this.gantt_end, 10, 'day');
+      this.gantt_start = date_utils.add(this.gantt_start, -20, 'day');
+      this.gantt_end = date_utils.add(this.gantt_end, 20, 'day');
     }
   }
 
